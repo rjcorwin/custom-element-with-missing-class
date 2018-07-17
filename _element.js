5b919741374dec8d58f28a01d15415788ae1b973
R.J. Steinert <rj@rjsteinert.com>
2018-07-16 21:57:14 -0400

    first

---
+++ _element.js
@@ -0,0 +1,32 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * `foo-element`
+ * 
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class FooElement extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'foo-element',
+      },
+    };
+  }
+}
+
+window.customElements.define('foo-element', FooElement);
